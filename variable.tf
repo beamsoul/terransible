8deeadc548f405103054f788ac42cf10efc232d1
beamsoul <nurzhanasamidinova@gmail.com>
2019-11-14 19:19:54 -0800

    variable mistakes detected

--- variable.tf
+++ variable.tf
@@ -1,9 +1,9 @@
-variable "aws_region"{}
-variable "aws_profile"{}
-data "aws_availability_zones" "available"{}
+variable "aws_region" {}
+variable "aws_profile" {}
+data "aws_availability_zones" "available" {}
 
-variable "vpc_cidr"{}
-variable "cidrs"{
+variable "vpc_cidr" {}
+variable "cidrs" {
   type = "map"
 }
 
